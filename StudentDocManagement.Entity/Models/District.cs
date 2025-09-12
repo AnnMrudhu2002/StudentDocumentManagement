8a0ceacd8e7d00a209b6f74bbb78e80da2948abb
AnnMrudhu2002 <snm20cs.annmrudhu@gmail.com>
2025-09-12 15:29:56 +0530

    .

---
+++ StudentDocManagement.Entity/Models/District.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentDocManagement.Entity.Models
+{
+    public class District
+    {
+        [Key]
+        public long DistrictId { get; set; }
+
+        [Required, MaxLength(100)]
+        public string DistrictName { get; set; } = string.Empty;
+
+        // Foreign key to State
+        public long StateId { get; set; }
+        [ForeignKey("StateId")]
+        //public State? State { get; set; }
+
+        public ICollection<PostOffice> PostOffices { get; set; } = new List<PostOffice>();
+    }
+}
---
+++ StudentDocManagement.Entity/Models/PostOffice.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentDocManagement.Entity.Models
+{
+    public class PostOffice
+    {
+        [Key]
+        public long PostOfficeId { get; set; }
+
+        [Required, MaxLength(100)]
+        public string PostOfficeName { get; set; } = string.Empty;
+
+        [MaxLength(10)]
+        public string Pincode { get; set; } = string.Empty;
+
+        // Foreign key to District
+        public long DistrictId { get; set; }
+        [ForeignKey("DistrictId")]
+        public District? District { get; set; }
+    }
+}
